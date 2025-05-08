469b8ff57104030eb1decde67f49698f8ee10d8b
haldaniko <haldaniko@gmail.com>
2025-05-08 18:29:50 +0300

    add 'Event' model

--- hashtaghub/models.py
+++ hashtaghub/models.py
@@ -11,3 +11,13 @@ class ContactMessage(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.email}"
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Име на събитието")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    datetime = models.DateTimeField(verbose_name="Дата и час")
+    location = models.CharField(max_length=255, verbose_name="Място на провеждане")
+
+    def __str__(self):
+        return f"{self.title} — {self.datetime.strftime('%d.%m.%Y %H:%M')}"
