d9284f5e0b05f49f441cd181fb38dc7920036519
haldaniko <haldaniko@gmail.com>
2025-05-08 15:05:02 +0300

    add model 'ContactMessage'

--- hashtaghub/models.py
+++ hashtaghub/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactMessage(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name="Име")
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    email = models.EmailField(verbose_name="Email")
+    message = models.TextField(verbose_name="Коментар")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.email}"
