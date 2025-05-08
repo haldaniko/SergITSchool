1cf78f2a68e4a5830d6922192096458a989ed210
haldaniko <haldaniko@gmail.com>
2025-05-08 15:06:01 +0300

    update 'index' view for contact form acception

--- hashtaghub/views.py
+++ hashtaghub/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .forms import ContactForm
 
 
 def index(request):
-    return render(request, "index.html")
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True})
+        return JsonResponse({'success': False, 'errors': form.errors}, status=400)
+
+    form = ContactForm()
+    return render(request, "index.html", {'form': form})
