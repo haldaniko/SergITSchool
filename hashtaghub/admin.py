5061866732a02f7d4cfba1e9b9860e9f4115e2c9
haldaniko <haldaniko@gmail.com>
2025-05-08 15:07:44 +0300

    register 'ContactMessage' in admin panel

--- hashtaghub/admin.py
+++ hashtaghub/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ContactMessage
 
-# Register your models here.
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'created_at')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
